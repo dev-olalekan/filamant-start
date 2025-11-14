43565f2dbfd52025badb927010eba9397657852e
olalekan <olalekanotuyiga@gmail.com>
2025-11-14 18:34:57 +0100

    documenting and adding icon

--- app/Filament/Resources/Countries/CountryResource.php
+++ app/Filament/Resources/Countries/CountryResource.php
@@ -18,7 +18,7 @@ class CountryResource extends Resource
 {
     protected static ?string $model = Country::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedFlag;
 
     public static function form(Schema $schema): Schema
     {
